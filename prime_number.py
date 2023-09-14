5eae957be5341f1942f131bbb4d19978aff8838d
Ruchit71 <51875187+Ruchit71@users.noreply.github.com>
2023-09-14 12:31:01 +0530

    Update prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -1,9 +1,9 @@
 #Prime numbers upto n in python
 def prime_number(lower, upper):
-  for num in range(lower,upper + 1):  
-     if num > 1:  
-         for i in range(2,num):  
-             if (num % i) == 0:  
-                 break  
-         else:  
-             print(num) 
+  for num in range(lower,upper + 1):
+    if num > 1:
+      for i in range(2,num):
+        if (num % i) == 0:
+          break
+        else:
+          print(num) 
